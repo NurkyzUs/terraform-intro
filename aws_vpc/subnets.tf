eba1e30a31d5ec724ef1770336403e9f69c52fd6
NurkyzUs <69085279+NurkyzUs@users.noreply.github.com>
2021-06-21 13:35:09 -0500

    Update subnets.tf

--- aws_vpc/subnets.tf
+++ aws_vpc/subnets.tf
@@ -1,8 +1,8 @@
 #### create a public subnet to associate with public RT
 
-resource "aws_subnet" "pub_subnet1" {
+resource "aws_subnet" "pub_subnet" {
   vpc_id     = aws_vpc.vpc_1.id
-  cidr_block = var.cidr_pub_sub1
+  cidr_block = var.cidr_pub_sub
 
   tags = local.common_tags
 }
@@ -10,9 +10,9 @@ resource "aws_subnet" "pub_subnet1" {
 
 #### create a private subnet to associate with private RT
 
-resource "aws_subnet" "pri_subnet1" {
+resource "aws_subnet" "pri_subnet" {
   vpc_id     = aws_vpc.vpc_1.id
-  cidr_block = var.cidr_pri_sub1
+  cidr_block = var.cidr_pri_sub
 
   tags = local.common_tags
 }
